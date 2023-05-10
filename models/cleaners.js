e9951e452034cfa028b593993037b41d2bcc0638
iSavedMyDiskHAHA <ck1130522@gmail.com>
2023-05-11 00:01:18 +0800

    origin time done

--- models/cleaners.js
+++ models/cleaners.js
@@ -15,13 +15,13 @@ class cleaner {
     }
 
     timeFormat(type, time) {
-        if (time == '') {return '0000-00-00';}
+        if (time == '' || time == null) {return '0000-00-00';}
         time = time.replaceAll('/', '-');
         if (time[6] == '-') {time = time.substring(0, 5) + '0' + time.substring(5);}
         if (time.lenght < 10 || time[9] == ' ') {time = time.substring(0, 8) + '0' + time.substring(8);}
         if (type == 3) {
-            if (time[19] != '-') {time = time.substring(0, 18) + '0' + time.substring(18);}
-            if (time.length != 23) {time = time.substring(0, 22) + '0' + time[21];}
+            if (time[19] == '-') {time = time.substring(0, 18) + '0' + time.substring(18);}
+            if (time.length != 23) {time = time.substring(0, 22) + '0' + time[22];}
         }
         return time;
     }
@@ -31,11 +31,11 @@ class cleaner {
         let start = -1;
         let end = -1;
         for (let j = 1; j < table[curRow].length; j++) {
-           if (table[curRow][j] == '卷件開始日期' || table[curRow][j] == 'date_from' || table[curRow][j] == '日期描述' || table[curRow][j] == '日期起') {
+           if (table[curRow][j] == '卷件開始日期' || table[curRow][j] == '"date_from' || table[curRow][j] == '"日期描述' || table[curRow][j] == '日期起') {
                 start = j;                
                 if ((type == 3) || (start > 0 && end > 0)) {break;}
            }
-           if (table[curRow][j] == '卷件結束日期' || table[curRow][j] == 'date_stop' || table[curRow][j] == '日期迄') {
+           if (table[curRow][j] == '卷件結束日期' || table[curRow][j] == '"date_stop' || table[curRow][j] == '日期迄') {
                 end = j;                
                 if (start > 0 && end > 0) {break;}
             }
@@ -51,7 +51,14 @@ class cleaner {
                 if (type >= 2) {
                     table[curRow][j] = table[curRow][j].substring(1);
                 }                                
-                if (j == start || j == end) {table[curRow][j] = this.timeFormat(type, table[curRow][j]);}
+                if (j == start) {
+                    table[curRow][0] = table[curRow][j];
+                    table[curRow][j] = this.timeFormat(type, table[curRow][j]);
+                }
+                if (j == end) {
+                    if (table[curRow][0].length > 1) {table[curRow][0] += ';' + table[curRow][j];}
+                    table[curRow][j] = this.timeFormat(type, table[curRow][j]);
+                }
             }
             curRow++;
         }
