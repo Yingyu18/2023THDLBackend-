e3a0f60cd65ebe18aa74d0bb13fcad38211a28d2
iSavedMyDiskHAHA <ck1130522@gmail.com>
2023-05-24 17:44:35 +0800

    89798

--- models/csv_model.js
+++ models/csv_model.js
@@ -22,21 +22,21 @@ class csvConverter {
         let results = type == 1 ? this.core : await tableFunc.openJsonHead(jid, 2);        
         let extra = results[0].length;
         let lines = 2;
-        contents.forEach(function(ele, index, ids) { 
-            console.log('contents = ' + contents[index]);
-            let table = cleaner.rawTable(contents[index]);console.log('table = ' + table);
-            if (type == 1) {table = cleaner.arrangeFormat(types[index], table, sidx[index]);}             
-            let corres = new Array (table[sidx[index]-1].length);
-            for (let i = 0; i < table[sidx[index]-1].length; i++) {
-                if (table[sidx[index]-1][i] == 'no') {corres[i] = -1;}
-                else if (results.indexOf(table[sidx[index]-1][i]) == -1) {
+        for (let k = 0; k < contents.length; k++) {
+            console.log('contents = ' + contents[k]);
+            let table = await cleaner.rawTable(contents[k]);console.log('table = ' + table);
+            if (type == 1) {table = await cleaner.arrangeFormat(types[k], table, sidx[k]);}             
+            let corres = new Array (table[sidx[k]-1].length);
+            for (let i = 0; i < table[sidx[k]-1].length; i++) {
+                if (table[sidx[k]-1][i] == 'no') {corres[i] = -1;}
+                else if (results.indexOf(table[sidx[k]-1][i]) == -1) {
                     corres[i] = extra;
-                    results[0].push(table[sidx[index]-1][i]);
-                    results[1].push('metadata/'+table[sidx[index]-1][i]);
+                    results[0].push(table[sidx[k]-1][i]);
+                    results[1].push('metadata/'+table[sidx[k]-1][i]);
                     extra++;
-                } else {corres[i] = results.indexOf(table[sidx[index]-1][i]);}
+                } else {corres[i] = results.indexOf(table[sidx[k]-1][i]);}
             }            
-            for (let i = sidx[index]-1; i < table.length; i++) {
+            for (let i = sidx[k]-1; i < table.length; i++) {
                 results.push(new Array(extra).fill(''));
                 for (let j = 0; j < table[i].length; j++) {
                     if (corres[j] < 0) {continue;}
@@ -49,7 +49,7 @@ class csvConverter {
                 if (type == 1) {results[lines][8] = table[i][0];}
                 lines++;
             } 
-        })
+        }
         for (let i = 2; i < results.length; i++) {
             if (results[i].length < results[0].length) {
                 results[i].push(new Array(results[0].length - results[i].length).fill(''));
