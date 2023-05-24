648e13537bcfc6ad50b76a65c8a2c8c2bf257325
iSavedMyDiskHAHA <ck1130522@gmail.com>
2023-05-25 04:15:17 +0800

    65789

--- models/cleaners.js
+++ models/cleaners.js
@@ -105,9 +105,16 @@ class cleaner {
         data = data.split('\n');  
         for (let i = 0; i < data.length; i++) {
             data[i] = data[i].split(',');
+        }console.log('no slice = ' + data);
+
+        console.log('slice = ' + data.slice(0, data.length-2));
+        if (data[data.length-1] == '') {
+            console.log('sliced!');
+            return data.slice(0, data.length-2);
         }
-        if (data[data.length-1] == '') {return data.slice(0, data.length-2);}
-        else {return data;} 
+        else {
+            return data;
+        } 
     }
 
     recover(str) {        
