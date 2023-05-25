fd4e10e9c5ca37cae15e9df2e60629958c82a8c8
iSavedMyDiskHAHA <ck1130522@gmail.com>
2023-05-25 09:03:58 +0800

    89798

--- models/cleaners.js
+++ models/cleaners.js
@@ -55,8 +55,8 @@ class cleaner {
         if (type == 3) {
             while (curRow < table.length) {           
                 table[curRow][0] = table[curRow][start]; console.log('org time = ' + table[curRow][start]);
-                let ttmp = table[curRow][start].substring(0, 10);console.log('stime = ' + ttmp);
-                let etmp = table[curRow][start].substring(13, 23);console.log('etime = ' + etmp);
+                var ttmp = table[curRow][start].substring(0, 10);console.log('stime = ' + ttmp);
+                var etmp = table[curRow][start].substring(13, 23);console.log('etime = ' + etmp);
                 table[curRow][start] = await this.timeFormat(ttmp);console.log('sstime = ' + table[curRow][start]);
                 table[curRow][end] =  await this.timeFormat(etmp);console.log('eetime = ' + table[curRow][end]);
                 curRow++;
