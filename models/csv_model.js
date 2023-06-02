1e7cb9738889e262a801671099e585148f3dbfc7
iSavedMyDiskHAHA <ck1130522@gmail.com>
2023-06-02 17:52:44 +0800

    csv con

--- models/csv_model.js
+++ models/csv_model.js
@@ -38,7 +38,7 @@ class csvConverter {
                     corres[i] = extra;
                     if (maps[k][i] == '') {results[0].push(table[sidx[k]-1][i]);}
                     else {results[0].push(maps[k][i]);}
-                    results[1].push('metadata/'+maps[k][i]);
+                    results[1].push('metadata/請替自訂欄位設定英文字');
                     extra++;
                 } else {corres[i] = results[0].indexOf(maps[k][i]);}
             }            
