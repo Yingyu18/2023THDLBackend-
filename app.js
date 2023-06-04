d871aa7ef08715d3f5e43ab0742a28b0d6d38bbe
DocuSkyGithub <docusky.contact@gmail.com>
2023-06-04 22:52:10 +0800

    128mb

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-// app.use(express.json({ limit: '1024mb' }));
-// app.use(express.urlencoded({ limit: '1024mb' }));
+app.use(express.json({ limit: '128mb' }));
+app.use(express.urlencoded({ limit: '128mb' }));
 app.use(bodyParser.json({ limit: '128mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '128mb' }));
 app.use(cookieParser());
