2f2af56df0436c072ac97122ede1aa9d7233f7e7
Yingyu18 <115443947+Yingyu18@users.noreply.github.com>
2023-06-04 18:05:27 +0800

    updateUserInfo response

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -291,7 +291,8 @@ const updateUserInfo = async (req, res) => {
             "institution": user.INSTITUTION,
             "title": user.TITLE,
             "researchTopics":user.RESEARCH_TOPIC,
-            "url":`http://140.112.30.230:3002/images/${req.user.userId}`
+            "avatar":`http://140.112.30.230:3002/images/${req.user.userId}`,
+            "verified":undefined
         })
     }
 };
