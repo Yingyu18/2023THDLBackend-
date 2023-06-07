1eda8e1449f120fa60144988ccad2b3e31057023
iSavedMyDiskHAHA <ck1130522@gmail.com>
2023-06-07 21:44:07 +0800

    qweqwe

--- controllers/project_controller.js
+++ controllers/project_controller.js
@@ -104,10 +104,7 @@ const getProjects = async (req, res) => {
         let { fileID, upload_time, fileName, updated, isMapped, isBuilt, description, sourceCsvs} = projects[i]
         console.log(projects[i])
         if (isMapped == 0) { isMapped = mapModel.checkProjectMappedOtherPlace(fileID)}
-        if(isMapped == true){isMapped=0}      
-        else{isMapped=1}
-        //return true if isMapped is still 0
-        // else if (isMapped is changed to 1)
+        
         upload_time = parseInt(upload_time)
         updated = parseInt(updated)
         let xml_id = projects[i].cores_xml_id
